3734cfbc73f98322468eb1f4e8aa575815603bb3
Guido Falk von Rudorff <guido@vonrudorff.de>
2015-08-27 15:23:22 +0100

    Basic program structure

--- src/bumblebee.py
+++ src/bumblebee.py
@@ -4,6 +4,7 @@
 # system modules
 import argparse
 import ConfigParser
+import hashlib
 import os
 
 # third-party modules
@@ -19,7 +20,8 @@ parser.add_argument('--bucket', type=str, help='Bucket to write the data to.')
 parser.add_argument('--sysconfig', type=str, default='~/.bumblebee', help='Configuration file.')
 parser.add_argument('--servername', type=str, default='default', help='The server alias in the config to write to.')
 parser.add_argument('--rolefile', type=str, default='.rolefile', help='The file specifying which data to import.')
-parser.add_argument('--runid', type=int, help='The running number of the run.')
+parser.add_argument('--runno', type=int, help='The running number of the run.')
+parser.add_argument('--token', type=str, help='Token for external bucket identification as string.')
 parser.add_argument('--resultsdir', type=str, default='.', help='The directory to read the data files from.')
 
 # counter (highest used values)
@@ -37,6 +39,11 @@ def _print(args, prefix, string=None):
 	if prefix.startswith('E'):
 		print 'Process aborted due to an unrecoverable error.'
 		exit(1)
+
+def _get_token(args):
+	if args.token is None:
+		return hashlib.md5(args.bucket).hexdigest()
+	return args.token
 
 def action_connectors(args):
 	_print(args, 'L0001', 'Available connectors:')
@@ -75,11 +82,25 @@ def action_upload(args):
 		_print(args, 'E0001', 'No rolefile specified.')
 	if args.bucket is None:
 		_print(args, 'E0002', 'No target bucket specified.')
-	if args.runid is None:
+	if args.runno is None:
 		_print(args, 'E0006', 'No run id specified.')
 
-	# connect to MongoDB
+	# connect to MongoDB and prepare bucket with run
 	db = get_server(args)
+	buckets = db.buckets
+	bucket_id = buckets.find_one({'name': args.bucket})
+	if bucket_id is None:
+		bucket_id = buckets.insert_one({
+			'name': args.bucket,
+			'token': _get_token(args)
+		}).inserted_id
+	runs = db.runs
+	run_id = runs.find_one({'bucket_id': bucket_id['_id'], 'number': args.runno})
+	if run_id is None:
+		run_id = runs.insert_one({
+			'bucket_id': bucket_id,
+			'number': args.runno
+		}).inserted_id
 
 	# select source connector
 	try:
@@ -98,9 +119,10 @@ def action_upload(args):
 	except:
 		_print(args, 'E0009', 'Invalid connector specified in role file.')
 
+	# parse data, write to database
 	try:
 		connector = connector(rolefile, directory=args.resultsdir)
-		connector.parse()
+		connector.parse(db, bucket_id['_id'], run_id['_id'])
 	except Exception as e:
 		_print(args, e.message)
 
--- src/connectors/cp2k.py
+++ src/connectors/cp2k.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# system modules
+import re
+
 # package modules
 import base
+
+def _find_by_re(lines, pattern, maxcount = None):
+	found = []
+	for line in lines:
+		res = re.findall(pattern, line)
+		if res != []:
+			found.append(res[0])
+		if maxcount is not None and len(found) >= maxcount:
+			break
+	if maxcount == 1:
+		if len(found) > 0:
+			return found[0]
+		else:
+			return None
+	return found
 
 class Cp2kConnector(base.Connector):
 	def __init__(self, rolefile, directory=None):
@@ -12,7 +30,20 @@ class Cp2kConnector(base.Connector):
 		self.add_filerole('logfile', at_least=1, at_most=1)
 		self._parse_rolefile(rolefile)
 
-	def parse(self):
+	def parse(self, db, bucket_id, run_id):
 		loglines = self._read_textfile_in_memory(self._filenames['logfile'][0])
 
-		print len(loglines)
+		db_run = db.runs.find_one({'_id': run_id})
+		try:
+			db_run['cores'] = int(_find_by_re(loglines, '^GLOBAL| Total number of message passing processes[ ]*(\d*)', 1))
+		except:
+			pass
+		try:
+			db_run['ensemble'] = _find_by_re(loglines, '^MD| Ensemble Type [ ]* ([^ ]*)', 1)
+		except:
+			pass
+		try:
+			db_run['ts'] = float(_find_by_re(loglines, '^MD| Time Step \[fs\] [ ]* (\d*\.\d*)', 1))
+		except:
+			pass
+		db.runs.update({'_id': run_id}, {'$set': db_run}, upsert=False)
