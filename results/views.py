125d3a4638b035d4bfd1747252bd5afb0c9804ca
Guido Falk von Rudorff <guido@vonrudorff.de>
2015-11-02 16:07:43 +0000

    Basic views for results

--- results/views.py
+++ results/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from results.models import *
 
-# Create your views here.
+def index(request):
+	template = loader.get_template('results/index.html')
+	systems = System.objects.all()
+	context = RequestContext(request, {
+		'systems': systems,
+	})
+	return HttpResponse(template.render(context))
