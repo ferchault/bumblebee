cf1d9573d5b6fd342c0b30fb07a2de0d4883e49b
ferchault <guido@vonrudorff.de>
2015-11-05 17:39:35 +0000

    Remaining agenda data structures

--- agenda/models.py
+++ agenda/models.py
@@ -5,6 +5,7 @@ from django.forms import ModelForm
 # app-specific imports
 from bumblebee.models import ExplainableMixin
 
+
 class Project(models.Model, ExplainableMixin):
 	name = models.CharField(max_length=45, verbose_name="Project Name", help_text="The name this project is referred to.")
 	active = models.BooleanField()
@@ -12,18 +13,34 @@ class Project(models.Model, ExplainableMixin):
 	def detailed(self):
 		return '%s %s' % (self._meta.verbose_name, self.name)
 
+
 class ProjectForm(ModelForm):
 	class Meta:
 		model = Project
 		fields = '__all__'
 
+
 class TodoStatus(models.Model, ExplainableMixin):
 	name = models.CharField(max_length=30)
 	completed = models.BooleanField()
 
+
+class TodoStatusForm(ModelForm):
+	class Meta:
+		model = TodoStatus
+		fields = '__all__'
+
+
 class TodoPriority(models.Model, ExplainableMixin):
 	name = models.CharField(max_length=20)
 	priority = models.IntegerField()
+
+
+class TodoPriorityForm(ModelForm):
+	class Meta:
+		model = TodoPriority
+		fields = '__all__'
+
 
 class TodoEntry(models.Model, ExplainableMixin):
 	task = models.CharField(max_length=200)
@@ -31,3 +48,9 @@ class TodoEntry(models.Model, ExplainableMixin):
 	duedate = models.DateField()
 	priority = models.ForeignKey(TodoPriority)
 	status = models.ForeignKey(TodoStatus)
+
+
+class TodoEntryForm(ModelForm):
+	class Meta:
+		model = TodoEntry
+		fields = '__all__'
--- agenda/views.py
+++ agenda/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-
+# django imports
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 
@@ -7,9 +6,11 @@ from django.core.urlresolvers import reverse_lazy
 from agenda.models import *
 from bumblebee.models import ModelNameMixin
 
+
 class ProjectListing(ModelNameMixin, ListView):
 	model = Project
 	template_name = 'listview-generic.html'
+
 
 class ProjectCreate(ModelNameMixin, CreateView):
 	model = Project
@@ -17,13 +18,90 @@ class ProjectCreate(ModelNameMixin, CreateView):
 	success_url = reverse_lazy('agenda-project-list')
 	template_name = 'createview-generic.html'
 
+
 class ProjectUpdate(ModelNameMixin, UpdateView):
 	model = Project
 	fields = ['name', 'active']
 	template_name = 'updateview-generic.html'
 	success_url = reverse_lazy('agenda-project-list')
 
+
 class ProjectDelete(ModelNameMixin, DeleteView):
 	model = Project
 	success_url = reverse_lazy('agenda-project-list')
+	template_name = 'deleteview-generic.html'
+
+
+class StatusListing(ModelNameMixin, ListView):
+	model = TodoStatus
+	template_name = 'listview-generic.html'
+
+
+class StatusCreate(ModelNameMixin, CreateView):
+	model = TodoStatus
+	form_class = TodoStatusForm
+	success_url = reverse_lazy('agenda-status-list')
+	template_name = 'createview-generic.html'
+
+
+class StatusUpdate(ModelNameMixin, UpdateView):
+	model = TodoStatus
+	fields = ['name', 'completed']
+	template_name = 'updateview-generic.html'
+	success_url = reverse_lazy('agenda-status-list')
+
+
+class StatusDelete(ModelNameMixin, DeleteView):
+	model = TodoStatus
+	success_url = reverse_lazy('agenda-status-list')
+	template_name = 'deleteview-generic.html'
+
+
+class PriorityListing(ModelNameMixin, ListView):
+	model = TodoPriority
+	template_name = 'listview-generic.html'
+
+
+class PriorityCreate(ModelNameMixin, CreateView):
+	model = TodoPriority
+	form_class = TodoPriorityForm
+	success_url = reverse_lazy('agenda-priority-list')
+	template_name = 'createview-generic.html'
+
+
+class PriorityUpdate(ModelNameMixin, UpdateView):
+	model = TodoPriority
+	fields = '__all__'
+	template_name = 'updateview-generic.html'
+	success_url = reverse_lazy('agenda-priority-list')
+
+
+class PriorityDelete(ModelNameMixin, DeleteView):
+	model = TodoPriority
+	success_url = reverse_lazy('agenda-priority-list')
+	template_name = 'deleteview-generic.html'
+
+
+class EntryListing(ModelNameMixin, ListView):
+	model = TodoEntry
+	template_name = 'listview-generic.html'
+
+
+class EntryCreate(ModelNameMixin, CreateView):
+	model = TodoEntry
+	form_class = TodoEntryForm
+	success_url = reverse_lazy('agenda-entry-list')
+	template_name = 'createview-generic.html'
+
+
+class EntryUpdate(ModelNameMixin, UpdateView):
+	model = TodoEntry
+	fields = ['name', 'completed']
+	template_name = 'updateview-generic.html'
+	success_url = reverse_lazy('agenda-entry-list')
+
+
+class EntryDelete(ModelNameMixin, DeleteView):
+	model = TodoEntry
+	success_url = reverse_lazy('agenda-entry-list')
 	template_name = 'deleteview-generic.html'
