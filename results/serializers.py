bc41408f47e40fd189b0e9e3eb584858508180ee
ferchault <guido@vonrudorff.de>
2015-11-16 17:59:02 +0000

    Accept numeric ID instead of URL for REST API

--- results/serializers.py
+++ results/serializers.py
@@ -2,139 +2,139 @@ from results.models import *
 from rest_framework import serializers
 
 
-class SystemSerializer(serializers.HyperlinkedModelSerializer):
+class SystemSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = System
-		fields = ('name',)
+		fields = ('id', 'name',)
 
 
-class BucketSerializer(serializers.HyperlinkedModelSerializer):
+class BucketSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Bucket
 		fields = ('name', 'token', 'comment', 'updated', 'system')
 
 
-class SeriesSerializer(serializers.HyperlinkedModelSerializer):
+class SeriesSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Series
 		fields = ('name', 'bucket')
 
 
-class SeriesAttributesSerializer(serializers.HyperlinkedModelSerializer):
+class SeriesAttributesSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = SeriesAttributes
 		fields = ('key', 'value', 'series')
 
 
-class SinglePointSerializer(serializers.HyperlinkedModelSerializer):
+class SinglePointSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = SinglePoint
 		fields = ('name', 'series')
 
 
-class SinglePointOuterSerializer(serializers.HyperlinkedModelSerializer):
+class SinglePointOuterSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = SinglePointOuter
 		fields = ('lagrangian', 'orderparameter', 'gradient', 'scfcycles', 'otnumber')
 
 
-class SinglePointAttributesSerializer(serializers.HyperlinkedModelSerializer):
+class SinglePointAttributesSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = SinglePointAttributes
 		fields = ('key', 'value', 'series')
 
 
-class MDRunSerializer(serializers.HyperlinkedModelSerializer):
+class MDRunSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = MDRun
 		fields = ('part', 'type', 'series')
 
 
-class MDStepSerializer(serializers.HyperlinkedModelSerializer):
+class MDStepSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = MDStep
 		fields = ('mdrun', 'stepnumber', 'steptime', 'masked')
 
 
-class AtomSerializer(serializers.HyperlinkedModelSerializer):
+class AtomSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Atom
 		fields = ('element', 'kind', 'system')
 
 
-class CoordinateSerializer(serializers.HyperlinkedModelSerializer):
+class CoordinateSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Coordinate
 		fields = ('x', 'y', 'z', 'atom', 'mdstep')
 
 
-class CoordinateWrappedSerializer(serializers.HyperlinkedModelSerializer):
+class CoordinateWrappedSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = CoordinateWrapped
 		fields = ('x', 'y', 'z', 'coordinate')
 
 
-class HirshfeldChargeSerializer(serializers.HyperlinkedModelSerializer):
+class HirshfeldChargeSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = HirshfeldCharge
 		fields = ('charge', 'alpha', 'beta', 'spin', 'reference', 'atom', 'mdstep')
 
 
-class ScaledCoordinateSerializer(serializers.HyperlinkedModelSerializer):
+class ScaledCoordinateSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = ScaledCoordinate
 		fields = ('x', 'y', 'z', 'atom', 'mdstep')
 
 
-class ScaledCoordinateWrappedSerializer(serializers.HyperlinkedModelSerializer):
+class ScaledCoordinateWrappedSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = ScaledCoordinateWrapped
 		fields = ('x', 'y', 'z', 'scaledcoordinate')
 
 
-class MullikenChargeSerializer(serializers.HyperlinkedModelSerializer):
+class MullikenChargeSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = MullikenCharge
 		fields = ('charge', 'alpha', 'beta', 'spin', 'atom', 'mdstep')
 
 
-class MDRunSettingsSerializer(serializers.HyperlinkedModelSerializer):
+class MDRunSettingsSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = MDRunSettings
 		fields = ('temperature', 'pressure', 'multiplicity', 'timestep', 'mdrun')
 
 
-class MDRunAttributesSerializer(serializers.HyperlinkedModelSerializer):
+class MDRunAttributesSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = MDRunAttributes
 		fields = ('key', 'value', 'mdrun')
 
 
-class StepCellSerializer(serializers.HyperlinkedModelSerializer):
+class StepCellSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = StepCell
 		fields = ('a', 'b', 'c', 'alpha', 'beta', 'gamma', 'mdstep')
 
 
-class StepEnsembleSerializer(serializers.HyperlinkedModelSerializer):
+class StepEnsembleSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = StepEnsemble
 		fields = ('temperature', 'pressure', 'volume', 'conserved', 'mdstep')
 
 
-class StepContributionsQMSerializer(serializers.HyperlinkedModelSerializer):
+class StepContributionsQMSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = StepContributionsQM
 		fields = ('coreselfenergy', 'corehamiltonian', 'hartree', 'xc', 'hfx', 'dispersion', 'constraint', 'mdstep')
 
 
-class StepEnergySerializer(serializers.HyperlinkedModelSerializer):
+class StepEnergySerializer(serializers.ModelSerializer):
 	class Meta:
 		model = StepEnergy
 		fields = ('etot', 'epot', 'ekin', 'drift', 'mdstep')
 
 
-class StepMetaQMSerializer(serializers.HyperlinkedModelSerializer):
+class StepMetaQMSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = StepMetaQM
 		fields = ('iasd', 's2', 'scfcycles', 'otcycles', 'mdstep')
